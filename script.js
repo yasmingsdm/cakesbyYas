e0493e1434a08bbb880b333a0feb68a5001e45aa
yasmingsdm <yasmin.dm@gmail.com>
2022-10-11 18:27:00 +0200

    changes with Vanessa

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
+const form = document.getElementById('form')
+form.onsubmit = order
 
 function order(){
-
+    event.preventDefault();
 // Get the infos from HTML:
 const kg = document.getElementById('kg').value
 const nome = document.getElementById('name').value
 
-const type = ()=>{if (document.getElementById('chocolate').checked ==true){
-     type = 'chocolate'
+let type = " "
+
+if (document.getElementById('chocolate').checked ==true){
+    type = 'chocolate'
 } else if (document.getElementById('redvelvet').checked == true){
     type = 'redvelvet'
 } else if (document.getElementById('strawberry').checked ==true){
     type = 'strawberry'
 } else if (document.getElementById('carrot').checked ==true){
     type = 'carrot'
-} else {alert('You must choose a cake')}}
+} else {alert('You must choose a cake')}
 
 // Calculate price and time to be ready:
 const price = kg*200
 
-const hour = (kg)=>{
+let hour = 0
     if (kg <=3){
         hour = 2
     } else{
         hour = 4
     }
-}
+
+    // Change images:
+
+let CakePicture = document.getElementById('CakePic')
+
+    
+if(type === 'chocolate') {
+    CakePicture.src = "./chocolate.jpg"
+} else if (type === 'redvelvet') {
+    CakePicture.src = "./red velvet.jpg"
+} else if (type === 'strawberry'){
+    CakePicture.src = "./strawberry.jpg"
+} else { CakePicture.src = "./carrot.jpg"}
+
     //Print on the screen:
 
-    let Text = ('Great ' + nome + 'your cake will be ready in ' + hour + ' and it will cost '+price +' SEK.')
-    document.write ('<h3>' + Text + '</h3>')
+    let Text = ('Great ' + nome + ' your cake will be ready in ' + hour + ' and it will cost '+price +' SEK.')
+    document.getElementById('h3').innerHTML = Text 
 }
 
-// Change images:
 
-const CakePic = (type) => {
-    if(type === 'chocolate') {
-        document.getElementById(CakePic).src = "./chocolate.jpg"
-    } else if (type === 'redvelvet') {
-        document.getElementById(CakePic).src = "./red velvet.jpg"
-    } else if (type === 'strawberry'){
-        document.getElementById(CakePic).src = "./strawberry.jpg"
-    } else { document.getElementById(CakePic).src = "./carrot.jpg"}
-}
+    
